d123695b989510f7c8598ab25fde0dbc4a3711e7
Nate Willard <nate.a.willard@gmail.com>
2017-02-04 18:35:02 -0800

    recursion quicksort

--- recursion.erl
+++ recursion.erl
@@ -1,5 +1,5 @@
 -module(recursion).
--export([fac/1, len/1, tail_fac/1, tail_len/1, duplicate/2, tail_dup/2, reverse/1, tail_reverse/1, sublist/2]).
+-export([fac/1, len/1, tail_fac/1, tail_len/1, duplicate/2, tail_dup/2, reverse/1, tail_reverse/1, sublist/2, zip/2, quicksort/1, lc_quicksort/1]).
 
 fac(N) when N == 1 -> N;
 fac(N) when N >= 1 -> N * fac(N-1).
@@ -86,6 +86,36 @@ sublist([H|T], N, Acc) when N > 0 ->
   sublist(T, N-1, [H|Acc]).
 
 
+% To push things a bit further, we'll write a zipping function. A zipping function will take two lists of same length as parameters and will join them as a list of tuples which all hold two terms. Our own zip/2 function will behave this way:
+%1> recursive:zip([a,b,c],[1,2,3]).
+%[{a,1},{b,2},{c,3}]
+
+zip(L1, L2) -> tail_reverse(tail_zip(L1, L2, [])).
+
+tail_zip([], [], Acc) -> Acc;
+tail_zip([], _, Acc) -> Acc;
+tail_zip(_, [], Acc) -> Acc;
+tail_zip([H1|T1], [H2|T2], Acc) ->
+  tail_zip(T1, T2, [{H1, H2}|Acc]).
+
+% I can (and will) now assume recursion and tail recursion make sense to you, but just to make sure, I'm going to push for a more complex example, quicksort. Yes, the traditional "hey look I can write short functional code" canonical example. A naive implementation of quicksort works by taking the first element of a list, the pivot, and then putting all the elements smaller or equal to the pivot in a new list, and all those larger in another list. We then take each of these lists and do the same thing on them until each list gets smaller and smaller. This goes on until you have nothing but an empty list to sort, which will be our base case. This implementation is said to be naive because smarter versions of quicksort will try to pick optimal pivots to be faster. We don't really care about that for our example though.
+
+% We will need two functions for this one: a first function to partition the list into smaller and larger parts and a second function to apply the partition function on each of the new lists and to glue them together. First of all, we'll write the glue function:
+
+% GLUE FN
+% quicksort([]) -> [];
+% quicksort([Pivot|Rest]) ->
+%   {Smaller, Larger} = partition(Pivot, Rest, [], []),
+%   quicksort(Smaller) ++ [Pivot] ++ quicksort(Larger).
+% 
+% % PARTITION FN
+% partition(_, [], Smaller, Larger) ->
+%   {Smaller, Larger};
+% partition(Pivot, [H|T], Smaller, Larger) ->
+%   if H =< Pivot -> partition(Pivot, T, [H|Smaller], Larger);
+%      H > Pivot -> partition(Pivot, T, Smaller, [H|Larger])
+%   end.
+% 
 
 
 
@@ -100,5 +130,35 @@ sublist([H|T], N, Acc) when N > 0 ->
 
 
 
+
+
+
+
+% FROM SCRATCH
+% How does quicksort work here?
+% We take in a list as input
+% We generate two lists, one of which is for smaller elements, one of which is for larger
+% We then glue them together smaller (sorted) + pivot + larger
+
+quicksort([]) -> [];
+quicksort([Pivot|Remainder]) ->
+  {Smaller, Larger} = partition(Pivot, Remainder, [], []),
+  quicksort(Smaller) ++ [Pivot] ++ quicksort(Larger).
+
+% How do we partition? We use tail recursion
+% We're given an array (remainder) that we need to allocate into the smaller and larger arrays
+partition(_, [], Smaller, Larger) -> {Smaller, Larger};
+partition(Pivot, [H|T], Smaller, Larger) ->
+  if H > Pivot ->
+       partition(Pivot, T, Smaller, [H|Larger]);
+     H =< Pivot ->
+       partition(Pivot, T, [H|Smaller], Larger)
+  end.
+
+% LIST COMPREHENSION VERSION
+% That's pretty cool actually
+lc_quicksort([]) -> [];
+lc_quicksort([Pivot|Rest]) ->
+  lc_quicksort([Smaller || Smaller <- Rest, Smaller < Pivot]) ++ [Pivot] ++ lc_quicksort([Larger || Larger <- Rest, Larger > Pivot]).
 
 
