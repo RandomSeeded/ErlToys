995ef007e5cca46a3cc6a2d87f27e7b01adcd28e
nate willard <nate.a.willard@gmail.com>
2017-03-19 10:05:16 -0700

    band sup simple_1f1

--- band_supervisor.erl
+++ band_supervisor.erl
@@ -5,6 +5,12 @@
 start_link(Type) ->
   supervisor:start_link({local, ?MODULE}, ?MODULE, Type).
 
+init(jamband) ->
+  {ok, {{simple_one_for_one, 3, 60},
+        [{jam_musician,
+          {musicians, start_link, []},
+          temporary, 1000, worker, [musicians]}
+        ]}};
 init(lenient) ->
   init({one_for_one, 3, 60});
 init(angry) ->
