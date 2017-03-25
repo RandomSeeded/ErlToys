cd833092566361d2aebf8698def9ec3f92263b9e
RandomSeeded <nate@blendlabs.com>
2017-03-24 17:09:59 -0700

    ppool api

---
+++ ppool.erl
@@ -0,0 +1,27 @@
+% Friendly API
+-module(ppool).
+-compile(export_all).
+
+start_link() ->
+  ppool_supersup:start_link().
+
+stop() ->
+  ppool_supersup:stop().
+
+start_pool(Name, Limit, {M,F,A}) ->
+  ppool_supersup:start_pool(Name, Limit, {M,F,A}).
+
+stop_pool(Name) ->
+  ppool_supersup:stop_pool(Name).
+
+run(Name, Args) ->
+  ppool_server:run(Name, Args).
+
+async_queue(Name, Args) ->
+  ppool_serv:async_queue(Name, Args).
+
+sync_queue(Name, Args) ->
+  ppool_serv:sync_queue(Name, Args).
+
+
+
