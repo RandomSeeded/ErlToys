f7db689203142b724f151910a327a39e97881db3
RandomSeeded <nate@blendlabs.com>
2017-03-24 17:07:03 -0700

    fin ppool serv

--- ppool_server.erl
+++ ppool_server.erl
@@ -53,14 +53,6 @@ init({Limit, MFA, Sup}) ->
   self() ! {start_worker_supervisor, Sup, MFA},
   {ok, #state{limit=Limit, refs=gb_sets:empty()}}. % Now the caller (ppool_sup) can continue.
 
-handle_info({start_worker_supervisor, Sup, MFA}, S = #state{}) ->
-  {ok, Pid} = supervisor:start_child(Sup, ?SPEC(MFA)),
-  link(Pid),
-  {noreply, S#state{sup=Pid}};
-handle_info(Msg, State) ->
-  io:format("Unknown msg: ~p~n", [Msg]),
-  {noreply, State}.
-
 handle_call({run, Args}, _From, S = #state{limit=N, sup=Sup, refs=R}) when N > 0 -> % limit represents the amount of remaining processes that can be added
   {ok, Pid} = supervisor:start_child(Sup, Args), % this doesn't mean start a supervisor. It means tell the supervisor (Sup) to start a child
   Ref = erlang:monitor(process, Pid),
@@ -70,7 +62,63 @@ handle_call({run, _Args}, _From, S=#state{limit=N}) when N =< 0 ->
 handle_call({sync, Args}, _From, S = #state{limit=N, sup=Sup, refs=R}) when N > 0 ->
   {ok, Pid} = supervisor:start_child(Sup, Args),
   Ref = erlang:monitor(process, Pid),
-  {reply, {ok, Pid}, S#state{limit=N-1, refs=gb_sets:add(Ref, R)}};
+  {reply, {ok, Pid}, S#state{limit=N-1, refs=gb_sets:add(Ref, R)}}; % for sync run, same thing, we run if we can
 handle_call({sync, Args}, From, S = #state{queue=Q}) ->
-  {noreply, S#state{queue=queue:in({From, Args}, Q)}}; 
+  {noreply, S#state{queue=queue:in({From, Args}, Q)}}; % but if we can't, we add it to the queue
+handle_call(stop, _From, State) ->
+  {stop, normal, ok, State};
+handle_call(_Msg, _From, State) ->
+  {noreply, State}.
+
+handle_cast({async, Args}, S=#state{limit=N, sup=Sup, refs=R}) when N > 0 ->
+  {ok, Pid} = supervisor:start_child(Sup, Args),
+  Ref = erlang:monitor(process, Pid),
+  {noreply, S#state{limit=N-1, refs=gb_sets:add(Ref, R)}};
+handle_cast({async, Args}, S=#state{limit=N, queue=Q}) when N =< 0 -> % async if we can't add to the queue as well
+  {noreply, S#state{queue=queue:in(Args,Q)}};
+handle_cast(_Msg, State) ->
+  {noreply, State}.
+
+% When do we REMOVE from the queue? When it goes down!
+handle_info({'DOWN', Ref, process, _Pid, _}, S = #state{refs=Refs}) ->
+  io:format("received down msg~n"),
+  case gb_sets:is_element(Refs, Refs) of
+    true ->
+      handle_down_worker(Ref, S);
+    false -> % why would we receive a message from something that's not our worker?? Unclear. I think this is just extra safety.
+      {noreply, S}
+  end;
+handle_info({start_worker_supervisor, Sup, MFA}, S = #state{}) ->
+  {ok, Pid} = supervisor:start_child(Sup, ?SPEC(MFA)),
+  link(Pid),
+  {noreply, S#state{sup=Pid}};
+handle_info(Msg, State) ->
+  io:format("Unknown msg: ~p~n", [Msg]),
+  {noreply, State}.
+
+
+handle_down_worker(Ref, S = #state{limit=L, sup=Sup, refs=Refs}) ->
+  case queue:out(S#state.queue) of
+    {{value, {From, Args}}, Q} -> % sync! Now we make sure to reply to unblock that thread.
+      {ok, Pid} = supervisor:start_child(Sup, Args),
+      NewRef = erlang:monitor(process, Pid),
+      NewRefs = gb_sets:insert(NewRef, gb_sets:delete(Ref, Refs)),
+      gen_server:reply(From, {ok, Pid}),
+      {noreply, S#state{refs=NewRefs, queue=Q}};
+    {{value, Args}, Q} -> % async, we just start and don't care.
+      {ok, Pid} = supervisor:start_child(Sup, Args),
+      NewRef = erlang:monitor(process, Pid),
+      NewRefs = gb_sets:insert(NewRef, gb_sets:delete(Ref, Refs)),
+      {noreply, S#state{refs=NewRefs, queue=Q}};
+    {empty, _} ->
+      {noreply, S#state{limit=L+1, refs=gb_sets:delete(Ref,Refs)}}
+  end.
+
+code_change(_OldVsn, State, _Extra) ->
+  {ok, State}.
+
+terminate(_Reason, _State) ->
+  ok.
+
+
 
